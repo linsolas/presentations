a9f379200d9167fb3ce373e13fe953d115f72e5b
linsolas <linsolas@gmail.com>
2017-03-07 12:22:16 +0100

    allow to split presentations slides in separate files

---
+++ revealjs/multi-files-plugin.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  activate();
+
+  function supportsImports() {
+    return 'import' in document.createElement('link');
+  }
+
+  function importPartials() {
+    const slideContainer = document.querySelector('#slides');
+    if (slideContainer === null) {
+      throw new Error('No slide container found (`#slides`)');
+    }
+
+    const links = document.querySelectorAll('link[rel="import"][role="slide"]');
+    for (let link of links) {
+      if (link.import) {
+        let sections = link.import.querySelectorAll('section.partial');
+        var count = 0;
+        for (let section of sections) {
+          count++;
+          slideContainer.appendChild(section.cloneNode(true));
+        }
+        if (count === 0) {
+          console.log(`No slide to add found in ${link.href} (selector "section.partial'")`);
+        } else {
+          console.log(`Content loaded from ${link.href}, ${count} slide(s) added.`);
+        }
+      }
+    }
+  }
+
+  function activate() {
+    if (supportsImports()) {
+      console.log('HTML Import supported!');
+    } else {
+      console.log('HTML Import not supported!');
+      return;
+    }
+
+    importPartials();
+  }
+})();
